c230de262414303033a94019318aa02c120a5b31
Kirill Marenov <kmarenov@gmail.com>
2015-04-05 22:04:28 +0300

    Status model

---
+++ backend/models/Status.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use common\models\User;
+
+/**
+ * This is the model class for table "status".
+ *
+ * @property integer $id
+ * @property string $status_name
+ * @property integer $status_value
+ *
+ * @property User[] $users
+ */
+class Status extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'status';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['status_name', 'status_value'], 'required'],
+            [['status_value'], 'integer'],
+            [['status_name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'status_name' => Yii::t('app', 'Status Name'),
+            'status_value' => Yii::t('app', 'Status Value'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::className(), ['status_id' => 'id']);
+    }
+}
