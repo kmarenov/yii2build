ae782828bfbeac7021f8cb8fa55e53ba6ed648da
Kirill Marenov <kmarenov@gmail.com>
2015-04-05 18:16:25 +0300

    role migration

---
+++ backend/models/Role.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "role".
+ *
+ * @property integer $id
+ * @property string $role_name
+ * @property integer $role_value
+ *
+ * @property User[] $users
+ */
+class Role extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'role';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['role_name', 'role_value'], 'required'],
+            [['role_value'], 'integer'],
+            [['role_name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'role_name' => Yii::t('app', 'Role Name'),
+            'role_value' => Yii::t('app', 'Role Value'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::className(), ['role_id' => 'id']);
+    }
+}
