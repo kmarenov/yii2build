0554e65790fac3fb6760bb80e57fe6c323d055bc
Kirill Marenov <kmarenov@gmail.com>
2015-04-05 23:10:10 +0300

    Update User Model with UserType

--- common/models/User.php
+++ common/models/User.php
@@ -11,6 +11,7 @@ use yii\helpers\Security;
 use yii\helpers\ArrayHelper;
 use backend\models\Role;
 use backend\models\Status;
+use backend\models\UserType;
 
 /**
  * User model
@@ -71,6 +72,7 @@ class User extends ActiveRecord implements IdentityInterface
             ['role_id', 'default', 'value' => 1],
             [['role_id'],'in', 'range'=>array_keys($this->getRoleList())],
             ['user_type_id', 'default', 'value' => 1],
+            [['user_type_id'],'in', 'range'=>array_keys($this->getUserTypeList())],
 
             ['username', 'filter', 'filter' => 'trim'],
             ['username', 'required'],
@@ -296,5 +298,37 @@ class User extends ActiveRecord implements IdentityInterface
     {
         $droptions = Status::find()->asArray()->all();
         return ArrayHelper::map($droptions, 'id', 'status_name');
+    }
+
+    public function getUserType()
+    {
+        return $this->hasOne(UserType::className(), ['id' => 'user_type_id']);
+    }
+
+    /**
+     * get user type name
+     *
+     */
+    public function getUserTypeName()
+    {
+        return $this->userType ? $this->userType->user_type_name : '- no user type -';
+    }
+
+    /**
+     * get list of user types for dropdown
+     */
+    public static function getUserTypeList()
+    {
+        $droptions = UserType::find()->asArray()->all();
+        return ArrayHelper::map($droptions, 'id', 'user_type_name');
+    }
+
+    /**
+     * get user type id
+     *
+     */
+    public function getUserTypeId()
+    {
+        return $this->userType ? $this->userType->id : 'none';
     }
 }
